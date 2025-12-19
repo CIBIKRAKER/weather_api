7d9ebf0bb27fe39e3b41273e6779e15101610867
CIBIKRAKER <apolatbusiness@gmail.com>
2025-12-19 22:12:28 +0100

    J

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,28 @@
 #Build and configure the app
-from flask import Flask
+from flask import Flask, render_template, request
+import weather as Weather
+
+weatherApp = Weather.WeatherAPI()
 
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template("weather.html")
 
-def hello_world():
-    return 'Hello World'
+@app.route("/weather", methods = ["POST", "GET"])
+def getWeather():
+
+    cityName = ""
+    weather = {}  
+
+    if request.method == "POST":
+        cityName = request.form["city"]
+        weather = weatherApp.fetch_weather(cityName)
+    
+    return render_template("result.html", cityName = cityName, weather = weather)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/weather.py
+++ app/weather.py
@@ -1,4 +1,5 @@
 #Weather API logic
+import datetime
 import os
 import requests
 import json
@@ -10,40 +11,25 @@ API_KEY = Config.API_KEY
 
 class WeatherAPI:
 
-    def __init__(self):
-        self.weather = {}
-
     def fetch_weather(self, city):
-        try:
-            if(os.path.exists("weather.json")):
-                with open("weather.json", "r") as file:
-                    self.weather = json.load(file)
-            else:
-                self.weather = {}
-        except ValueError:
-            self.weather = {}
-            
-
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-
         response = requests.get(url)
-
+        weather_info = {}
         if response.status_code == 200:
             data = response.json()
+            temperature = data["main"]["temp"]
+            temp_min = data["main"]["temp_min"]
+            temp_max = data["main"]["temp_max"]
             weather_info = {
-                "city": data["name"],
-                "temperature": data["main"]["temp"],
-                "description": data["weather"][0]["description"],
-                "humidity": data["main"]["humidity"],
-                "wind_speed": data["wind"]["speed"]
-            }
-        self.weather = weather_info
-
-        with open("weather.json", "w") as file:
-            json.dump(self.weather, indent=4, fp=file)
-
-        print(self.weather)
-        return self.weather
+            "city": data["name"],
+            "temperature": round(temperature),
+            "temp_min": round(temp_min),
+            "temp_max": round(temp_max),
+            "description": data["weather"][0]["description"],
+            "main": data["weather"][0]["main"],   
+            "date": str(datetime.datetime.now().strftime("%A, %B %d"))
+        }
+        return weather_info
     
     
 
