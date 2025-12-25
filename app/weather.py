8966f0670b609c89377776272d4b3d4c932974d5
CIBIKRAKER <apolatbusiness@gmail.com>
2025-12-25 17:49:05 +0100

    Finished

--- app/weather.py
+++ app/weather.py
@@ -1,10 +1,14 @@
-#Weather API logic
-import datetime
-import os
-import requests
+import redis
 import json
+import datetime
+import requests
 from config import Config
 
+redis_client = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
 
 API_KEY = Config.API_KEY
 
@@ -12,34 +16,47 @@ API_KEY = Config.API_KEY
 class WeatherAPI:
 
     def fetch_weather(self, city):
-        try:
-            url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-        except Exception as e:
-            print(f"Error constructing URL: {e}")
-            return None
+        city = city.lower().strip()
+
+        # cache key (hourly)
+        current_hour = datetime.datetime.now().strftime("%Y-%m-%d-%H")
+        cache_key = f"weather:{city}:{current_hour}"
+
         
-        try:
-            response = requests.get(url)
-        except requests.RequestException as e:
-            print(f"Error fetching weather data: {e}")
-            return None
+        cached_data = redis_client.get(cache_key)
+        if cached_data:
+            print(f"Using cached weather for {city}")
+            return json.loads(cached_data)
+        else:
+            print(f"Fetching new weather for {city}")
         
-        weather_info = {}
-        if response.status_code == 200:
-            data = response.json()
-            temperature = data["main"]["temp"]
-            temp_min = data["main"]["temp_min"]
-            temp_max = data["main"]["temp_max"]
-            weather_info = {
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+        response = requests.get(url)
+        if response.status_code != 200:
+            return None
+
+        data = response.json()
+
+        weather_info = {
             "city": data["name"],
-            "temperature": round(temperature),
-            "temp_min": round(temp_min),
-            "temp_max": round(temp_max),
+            "temperature": round(data["main"]["temp"]),
+            "temp_min": round(data["main"]["temp_min"]),
+            "temp_max": round(data["main"]["temp_max"]),
             "description": data["weather"][0]["description"],
-            "main": data["weather"][0]["main"],   
-            "date": str(datetime.datetime.now().strftime("%A, %B %d, %H:00"))
+            "main": data["weather"][0]["main"],
+            "date": datetime.datetime.now().strftime("%A, %B %d, %H:00")
         }
+
+        
+        redis_client.setex(
+            cache_key,
+            3600,
+            json.dumps(weather_info)
+        )
+
         return weather_info
+
     
     
 
